98f4b23bf74aecbd6477d9a0cdd2ed84040202bf
Ramazoid <krockodil@mail.ru>
2022-11-08 12:49:12 +0500

    Murzin Test

---
+++ Assets/Changer.cs
@@ -0,0 +1,25 @@
+﻿using AxGrid.Base;
+using AxGrid.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Changer : MonoBehaviourExtBind
+{
+    private Text TextIndicator;
+
+    [OnStart]
+    private void OnServerInitialized()
+    {
+        TextIndicator = GetComponent < Text > ();  
+
+    }
+    [Bind("OnMetavarChanged")]
+    void Changed(string val)
+    {
+        TextIndicator.text = val;
+    }
+
+
+}
---
+++ Assets/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using AxGrid.Base;
+using AxGrid.Model;
+using AxGrid.Path;
+using UnityEngine;
+
+public class StartUp : MonoBehaviourExt
+{
+    [OnStart]
+    private void Init()
+    {
+        Model.Set("MetaVar", 0);
+
+    }
+   
+    public void Inc()
+    {
+        Model.Inc("Metavar");
+    }
+    public void Dec()
+    {
+        Model.Dec("Metavar");
+    }
+}
